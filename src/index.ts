321b31336ce09da7a4a2ad3cc4f689c66bfeaace
Sergio <sergioborrelli@hotmail.co.uk>
2021-04-14 06:59:38 +0100

    Removed unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import {Inject, Container, Scope} from 'typescript-ioc'
+import {Container} from 'typescript-ioc'
 import {Summary} from "./Summary";
-import {CheckNumbers} from "./CheckNumbers";
-import {ConsoleReport} from "./ConsoleReport";
 
 let summary = Container.get(Summary);
 summary.loopThroughNumber(1, 100)
